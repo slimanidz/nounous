854c2a8290215ad5ee04be94b0fe92d88ee6e878
Mohammed SLIMANI <mslimani62@yahoo.fr>
2023-05-11 13:42:49 +0200

    migrate before build

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,7 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
   },
   migrations: {
     tableName: "knex_migrations",
