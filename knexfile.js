55ea5b31d1962d4af343cb114e55dd7fa2768a66
Mohammed SLIMANI <mslimani62@yahoo.fr>
2023-05-21 14:44:29 +0200

    Best Design1

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,13 @@ module.exports = {
   client: "pg",
   // pour vercel connexion:process.env.DB_URL
   // Pour le local il faut mettre tous
-  connection: {
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-  },
-  // process.env.DB_URL,
+  connection: process.env.DB_URL,
+  // {
+  //   user: process.env.DB_USER,
+  //   database: process.env.DB_DATABASE,
+  //   password: process.env.DB_PASSWORD,
+  // },
+
   migrations: {
     tableName: "knex_migrations",
     directory: `${process.cwd()}/src/api/db/migrations`,
