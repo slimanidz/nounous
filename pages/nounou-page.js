14973e7cf1a1ce7d7a7dc1e5b81bb5c26fb109a8
Mohammed SLIMANI <mslimani62@yahoo.fr>
2026-05-25 17:53:45 +0200

    sepprission de Nounous-page

--- pages/nounou-page.js
+++ pages/nounou-page.js
@@ -2,11 +2,7 @@ import React from "react";
 import Nounou from "../components/Nounou";
 
 const NounouPage = () => {
-  return (
-    <div>
-      <Nounou />
-    </div>
-  );
+  return <div>{/* <Nounou /> */}</div>;
 };
 
 export default NounouPage;
